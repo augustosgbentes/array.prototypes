e6bb512d47c866c8f8f771dd3a99a6fc7aa5d713
augustosgbentes <augustobentes@hotmail.com>
2023-08-17 02:22:26 -0300

    Exercicios sobre metodos de array

--- index.js
+++ index.js
@@ -88,5 +88,5 @@ console.log(arrAnimaisLength);
 
 const numeros = [1,2,3,4,5,6,7,8,9,10];
 
-const numerosImpares = numeros.filter(numero => numero % 2 != 0)
-console.log(numerosImpares);
+const numerosImpares = numeros.filter(numero => numero % 2 != 0);
+console.log(numerosImpares); 
